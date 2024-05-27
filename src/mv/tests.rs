f1cfaf1397d8eeb762778bfe6f3dc74d31067105
cowboy8625 <cowboy8625@yahoo.com>
2024-05-27 09:05:06 -0500

    impl test for or instruction

--- src/mv/tests.rs
+++ src/mv/tests.rs
@@ -17,6 +17,26 @@ fn test_load() -> Result<()> {
     for i in 1..Mv::REGESTER_COUNT {
         assert_eq!(mv.get_regester(i as u8), &0);
     }
-    assert_eq!(mv.get_regester(0), &100);
+    Ok(())
+}
+
+#[test]
+fn test_or() -> Result<()> {
+    let src = r#"
+    .entry main
+    main:
+        load[u8] %0 1
+        load[u8] %1 2
+        or[u8] %0 %0 %1
+        hult
+"#;
+    let program = asm::assemble(src)?;
+    let mut mv = Mv::new(program)?;
+    mv.run()?;
+    assert_eq!(mv.get_regester(0), &3);
+    assert_eq!(mv.get_regester(1), &2);
+    for i in 2..Mv::REGESTER_COUNT {
+        assert_eq!(mv.get_regester(i as u8), &0);
+    }
     Ok(())
 }
